72d426e7ebeaf7e8f8917f2aa40472826bf9f801
Mads Frank <madsf88@gmail.com>
2015-03-10 21:42:14 +0100

    Small css and template changes

--- wp/wp-content/themes/FoundationPress-master/page-event.php
+++ wp/wp-content/themes/FoundationPress-master/page-event.php
@@ -6,15 +6,11 @@ get_header(); ?>
 <?php $cat_id = 4; //the certain category ID
     $latest_cat_post = new WP_Query( array('posts_per_page' => 1, 'category__in' => array($cat_id)));
     if( $latest_cat_post->have_posts() ) : while( $latest_cat_post->have_posts() ) : $latest_cat_post->the_post();  ?>
-    <div style="background: url(<?php the_field('background_image'); ?>); height:100vh; width: 100%; background-size: cover;  background-position: center; position: fixed; top: 0;">
+    <div style="background: url(<?php the_field('background_image'); ?>); height:100vh; width: 100%; background-size: cover; z-index:-1; background-position: center; position: fixed; top: 0;">
     </div>
-    <section class="container" role="document">
-        <div class="row">
+    <!--<section class="container" role="document">-->
+        <div class="row" style="height: 100vh;">
             <div class="large-12 columns">
-                <nav></nav>
-                <div class="logo">
-                    ITU.film
-                </div>
                 <div class="trailer"></div>
                 <div class="scrollDown"></div>
                 <div class="heading">
@@ -24,7 +20,7 @@ get_header(); ?>
             </div>        
         </div>
         <div class="row">
-            <div class="small-12 medium-8 large-8 small-centered columns movieInfo">
+            <div class="small-12 medium-7 large-6 columns small-centered movieInfo">
                 <div class="panel">
                     <link href="http://addtocalendar.com/atc/1.5/atc-style-blue.css" rel="stylesheet" type="text/css">
                     <!-- 2. Include script -->
@@ -36,16 +32,37 @@ get_header(); ?>
                                 s.src = ('https:' == window.location.protocol ? 'https' : 'http')+'://addtocalendar.com/atc/1.5/atc.min.js';
                                 var h = d[g]('body')[0];h.appendChild(s); }})();
                     </script>
+                     <script type="text/javascript"> 
+                        ( function( $ ) {
+                            var imdbURL = "<?php  the_field('imdb_url'); ?>";
+                            var imdbURL = imdbURL.replace('www.imdb.com/title/','')
+                            var imdbURL = imdbURL.replace(imdbURL.substr(imdbURL.lastIndexOf('/')), '');
+                            var imdbAPI = "http://www.omdbapi.com/?i="+imdbURL+"&plot=full&r=json";
+                            $.getJSON(imdbAPI, function (json) {
+                                var rating = json.imdbRating;
+                                $('#rating').text(rating);
 
+                                var director = json.Director;
+                                $('#director').text(director);
+
+                                var year = json.Year;
+                                $('#year').text(year);
+
+                                var startTime = ("<?php  the_field('start_time'); ?>");
+                                var duration = json.Runtime;
+                                $('#duration').text(duration);
+                            }); 
+                        } )( jQuery );
+                    </script>
                     <dl>
                         <dt>Director: </dt>
-                        <dd id="director">Christogher Nolan</dd>
+                        <dd id="director"></dd>
                         <dt>Running Time: </dt>
-                        <dd id="duration">2pm - 4:30pm</dd>
-                        <dt>IMDb scrore: </dt>
-                        <dd id="score">8.8</dd>
+                        <dd id="duration"></dd>
+                        <dt>IMDb rating: </dt>
+                        <dd id="rating"></dd>
                         <dt>Year: </dt>
-                        <dd id="year">2014</dd>
+                        <dd id="year"></dd>
                     </dl>
                     <ul>
                         <li class="icons">
@@ -79,10 +96,10 @@ get_header(); ?>
             </div>
         </div>
 
-        <video autobuffer controls autoplay style="z-index: 999; position: relative; height: 100vh">
+        <!--<video autobuffer controls autoplay style="z-index: 999; position: relative; height: 100vh">
           <source id="mp4" src="<?php the_field('trailer_url'); ?>" type="video/mp4">
-        </video>
-    </section>
+        </video>-->
+    <!--</section>-->
 <?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
