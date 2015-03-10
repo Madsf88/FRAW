4da5c3f29ecdc37bf138e2e7ad46e85b27bc2c12
Mads Frank <madsf88@gmail.com>
2015-03-10 17:11:23 +0100

    Cabin web-font

--- wp/wp-includes/theme.php
+++ wp/wp-includes/theme.php
@@ -2017,6 +2017,20 @@ function wp_customize_support_script() {
 			b[c] += ( window.postMessage && request ? ' ' : ' no-' ) + cs;
 		}());
 	</script>
+	<script type="text/javascript">
+      WebFontConfig = {
+        google: { families: [ 'Cabin:400,500,600,700,400italic,500italic,600italic,700italic:latin' ] }
+      };
+      (function() {
+        var wf = document.createElement('script');
+        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+          '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+        wf.type = 'text/javascript';
+        wf.async = 'true';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(wf, s);
+      })(); 
+      </script>
 	<?php
 }
 
