766dfb2e0fa8d9b24ae5b9fac323753bc56dd916
Mads Frank <madsf88@gmail.com>
2015-03-11 11:42:50 +0100

    Google fonts kald

---
+++ wp/wp-content/themes/FoundationPress-child/footer.php
@@ -0,0 +1,29 @@
+</section>
+<footer class="row">
+	<?php do_action('foundationPress_before_footer'); ?>
+	<?php dynamic_sidebar("footer-widgets"); ?>
+	<?php do_action('foundationPress_after_footer'); ?>
+</footer>
+<a class="exit-off-canvas"></a>
+
+	<?php do_action('foundationPress_layout_end'); ?>
+	</div>
+</div>
+<?php wp_footer(); ?>
+<?php do_action('foundationPress_before_closing_body'); ?>
+<script type="text/javascript">
+      WebFontConfig = {
+        google: { families: [ 'Cabin:400,500,600,700,400italic,500italic,600italic,700italic:latin' ] }
+      };
+      (function() {
+        var wf = document.createElement('script');
+        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+          '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+        wf.type = 'text/javascript';
+        wf.async = 'true';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(wf, s);
+      })(); 
+</script>
+</body>
+</html>
