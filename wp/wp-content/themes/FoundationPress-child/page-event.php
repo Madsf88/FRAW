00209c4fe5a7708c03b977307aab6ffc7f78ded7
Chris Toft Christensen <spam@chrisc.dk>
2015-03-12 08:53:54 +0100

    DB + plus author change

--- wp/wp-content/themes/FoundationPress-child/page-event.php
+++ wp/wp-content/themes/FoundationPress-child/page-event.php
@@ -102,9 +102,9 @@ get_header(); ?>
 <div class="row">
     <div class="small-12 medium-8 large-8 small-centered columns review">
         <div class="panel">
-            <h1 class="review__h1"><?php the_field('review_title'); ?> lorem ipsum dolor sit amet</h1>
+            <h1 class="review__h1"><?php the_field('review_title'); ?></h1>
             <?php the_content(); ?>
-            <a href="#authorID<?php the_author_ID(); ?>"><?php the_author(); ?></a>
+            This review was brought to you by <a href="#authorID<?php the_author_ID(); ?>"><?php the_author(); ?></a>
         </div>
     </div>
 </div>
