791cb9d42b9949699622dbebcd5d5a6a13045b68
Chris Toft Christensen <spam@chrisc.dk>
2015-03-14 18:54:09 +0100

    Fixed indent for easier overview

--- wp/wp-content/themes/FoundationPress-child/footer.php
+++ wp/wp-content/themes/FoundationPress-child/footer.php
@@ -1,56 +1,55 @@
-</section>
-<footer class="row">
-	<?php do_action('foundationPress_before_footer'); ?>
-	<?php dynamic_sidebar("footer-widgets"); ?>
-	<?php do_action('foundationPress_after_footer'); ?>
-</footer>
-<a class="exit-off-canvas"></a>
+    </section>
+    <footer class="row">
+        <?php do_action('foundationPress_before_footer'); ?>
+        <?php dynamic_sidebar("footer-widgets"); ?>
+        <?php do_action('foundationPress_after_footer'); ?>
+    </footer>
+    <a class="exit-off-canvas"></a>
 
-	<?php do_action('foundationPress_layout_end'); ?>
-	</div>
+    <?php do_action('foundationPress_layout_end'); ?>
 
-<?php wp_footer(); ?>
-<?php do_action('foundationPress_before_closing_body'); ?>
-<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ; ?>/js/script.js"></script>
+    <?php wp_footer(); ?>
+    <?php do_action('foundationPress_before_closing_body'); ?>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() ; ?>/js/script.js"></script>
 
-<!-- Slideout.js navigation -->
+    <!-- Slideout.js navigation -->
 
-<script src="<?php echo get_stylesheet_directory_uri() ; ?>/js/slideout.js"></script>
-<script>
-    var slideout = new Slideout({
-        'panel': document.getElementById('wrapper'),
-        'menu': document.getElementById('menu'),
-        'padding': 256,
-        'tolerance': 70
-    });
-    
-    $(document).ready(function() {
-        $(".menu-icon").click(function() {
-            slideout.toggle();
+    <script src="<?php echo get_stylesheet_directory_uri() ; ?>/js/slideout.js"></script>
+    <script>
+        var slideout = new Slideout({
+            'panel': document.getElementById('wrapper'),
+            'menu': document.getElementById('menu'),
+            'padding': 256,
+            'tolerance': 70
         });
-    });
-</script>
 
-<!-- Slideout.js navigation - END -->
+        $(document).ready(function() {
+            $(".menu-icon").click(function() {
+                slideout.toggle();
+            });
+        });
+    </script>
 
-<!-- Web fonts -->
+    <!-- Slideout.js navigation - END -->
 
-<script type="text/javascript">
-      WebFontConfig = {
-        google: { families: [ 'Cabin:400,500,700,400italic:latin', 'EB+Garamond::latin' ] }
-      };
-      (function() {
-        var wf = document.createElement('script');
-        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-          '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-        wf.type = 'text/javascript';
-        wf.async = 'true';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(wf, s);
-      })(); 
-</script>
+    <!-- Web fonts -->
 
-<!-- Web fonts - END -->
+    <script type="text/javascript">
+          WebFontConfig = {
+            google: { families: [ 'Cabin:400,500,700,400italic:latin', 'EB+Garamond::latin' ] }
+          };
+          (function() {
+            var wf = document.createElement('script');
+            wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+              '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+            wf.type = 'text/javascript';
+            wf.async = 'true';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(wf, s);
+          })(); 
+    </script>
+
+    <!-- Web fonts - END -->
 
 </body>
 </html>
--- wp/wp-content/themes/FoundationPress-child/header.php
+++ wp/wp-content/themes/FoundationPress-child/header.php
@@ -1,60 +1,40 @@
 <!doctype html>
 <html class="no-js" <?php language_attributes(); ?> >
-	<head>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<title><?php if ( is_category() ) {
-			echo single_cat_title();
-		} elseif ( is_tag() ) {
-			echo 'Tag Archive for &quot;'; single_tag_title(); echo '&quot; | '; bloginfo( 'name' );
-		} elseif ( is_archive() ) {
-			wp_title(''); echo ' Archive | '; bloginfo( 'name' );
-		} elseif ( is_search() ) {
-			echo 'Search for &quot;'.esc_html($s).'&quot; | '; bloginfo( 'name' );
-		} elseif ( is_home() || is_front_page() ) {
-			bloginfo( 'name' ); echo ' | '; bloginfo( 'description' );
-		}  elseif ( is_404() ) {
-			echo 'Error 404 Not Found | '; bloginfo( 'name' );
-		} elseif ( is_single() ) {
-			wp_title('');
-		} else {
-			echo wp_title( ' | ', 'false', 'right' ); bloginfo( 'name' );
-		} ?></title>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php if ( is_category() ) {
+        echo single_cat_title();
+    } elseif ( is_tag() ) {
+        echo 'Tag Archive for &quot;'; single_tag_title(); echo '&quot; | '; bloginfo( 'name' );
+    } elseif ( is_archive() ) {
+        wp_title(''); echo ' Archive | '; bloginfo( 'name' );
+    } elseif ( is_search() ) {
+        echo 'Search for &quot;'.esc_html($s).'&quot; | '; bloginfo( 'name' );
+    } elseif ( is_home() || is_front_page() ) {
+        bloginfo( 'name' ); echo ' | '; bloginfo( 'description' );
+    }  elseif ( is_404() ) {
+        echo 'Error 404 Not Found | '; bloginfo( 'name' );
+    } elseif ( is_single() ) {
+        wp_title('');
+    } else {
+        echo wp_title( ' | ', 'false', 'right' ); bloginfo( 'name' );
+    } ?></title>
 
-		<link rel="icon" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/favicon.ico" type="image/x-icon">
-		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/apple-touch-icon-144x144-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/apple-touch-icon-114x114-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/apple-touch-icon-72x72-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/apple-touch-icon-precomposed.png">
-		
-		<?php wp_head(); ?>
-	</head>
-	<body <?php body_class(); ?>>
-	<?php do_action('foundationPress_after_body'); ?>
-	
-<!--	<div class="off-canvas-wrap" data-offcanvas>-->
-	<?php wp_nav_menu(array('container_id' => 'menu')); ?>
-	<div class="inner-wrap" id="wrapper">
-        
-	
-	<?php do_action('foundationPress_layout_start'); ?>
-	
-	
-	
-<!--
-	<nav class="tab-bar">
-		<section class="left-small">
-			<a class="left-off-canvas-toggle menu-icon" href="#"><span></span></a>
-		</section>
-		<p class="right title"><?php bloginfo( 'name' ); ?></p>
+    <link rel="icon" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/apple-touch-icon-144x144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/apple-touch-icon-114x114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/apple-touch-icon-72x72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/apple-touch-icon-precomposed.png">
 
-	</nav>
--->
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <?php do_action('foundationPress_after_body'); ?>
+    <?php wp_nav_menu(array('container_id' => 'menu')); ?>
 
-	<?php get_template_part('parts/off-canvas-menuNO'); ?>
+   <?php do_action('foundationPress_layout_start'); ?>
 
-	<?php get_template_part('parts/top-barNO'); ?>
-
-<section class="container" role="document">
-    <div class="menu-icon"></div>
-	<?php do_action('foundationPress_after_header'); ?>
+    <section class="container" id="wrapper" role="document">
+        <div class="menu-icon"></div>
+        <?php do_action('foundationPress_after_header'); ?>
