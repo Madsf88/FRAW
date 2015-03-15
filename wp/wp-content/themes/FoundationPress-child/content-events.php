d0f97570221ab9f1492669dabbd635134acd6806
Mads Frank <madsf88@gmail.com>
2015-03-15 14:06:05 +0100

    Forsøg på at lave en "Past event" section.

--- wp/wp-content/themes/FoundationPress-child/content-events.php
+++ wp/wp-content/themes/FoundationPress-child/content-events.php
@@ -8,15 +8,35 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <a href="<?php the_permalink(); ?>">
-        <div>
-            <?php 
-                if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-                    the_post_thumbnail('large');
-                } 
-            ?>
-            <h3><?php the_title(); ?></h3>
-            <p><?php  the_field('date'); ?></p>
-        </div>
-    </a>
+    
+    <?php
+        $eventdate = get_field('calendar_data');
+        $date = new DateTime($eventdate);
+        $now = new DateTime();
+        
+        
+
+        if($date < $now) {
+                echo '<div style="opacity:0.3">';
+                echo '<a href="'. get_permalink().'">
+                    <div>
+                        '. the_post_thumbnail('large') .'
+                        <h3>'. get_the_title() .'</h3>
+                        <p>'.  get_field('date') .'</p>
+                    </div>
+                
+                   </a>';
+                   echo "</div>";
+        }else 
+                echo '<a href="'. get_permalink().'">
+                    <div>
+                        '. the_post_thumbnail('large') .'
+                        <h3>'. get_the_title() .'</h3>
+                        <p>'.  get_field('date') .'</p>
+                    </div>
+                
+                   </a>';
+    ?>
+    
+    
 </article>
