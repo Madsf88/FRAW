9e44178225db3ef400cad32553bd5f5059790500
Mads Frank <madsf88@gmail.com>
2015-03-17 00:10:32 +0100

    Fav icon

--- wp/wp-content/themes/FoundationPress-child/header.php
+++ wp/wp-content/themes/FoundationPress-child/header.php
@@ -21,7 +21,7 @@
         echo wp_title( ' | ', 'false', 'right' ); bloginfo( 'name' );
     } ?></title>
 
-    <link rel="icon" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="<?php echo get_stylesheet_directory_uri() ; ?>/img/icons/favicon.ico" type="image/x-icon">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/apple-touch-icon-144x144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/apple-touch-icon-114x114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_stylesheet_directory_uri() ; ?>/assets/img/icons/apple-touch-icon-72x72-precomposed.png">
