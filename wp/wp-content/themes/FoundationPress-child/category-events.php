de95223f55b5bb9266acf47afefbeae946ecff87
Chris Toft Christensen <spam@chrisc.dk>
2015-03-28 22:39:55 +0100

    Event list is now individual cards

--- wp/wp-content/themes/FoundationPress-child/category-events.php
+++ wp/wp-content/themes/FoundationPress-child/category-events.php
@@ -19,19 +19,17 @@
     </div>
     <div class="row">
         <div class="small-12 medium-8 large-8 small-centered columns eventList removePadding">
-            <div class="panel">
-                <?php if ( have_posts() ) : ?>
+            <?php if ( have_posts() ) : ?>
 
-                    <?php /* Start the Loop */ ?>
-                    <?php while ( have_posts() ) : the_post(); ?>
-                        <?php get_template_part( 'content-events', get_post_format() ); ?>
-                    <?php endwhile; ?>
+                <?php /* Start the Loop */ ?>
+                <?php while ( have_posts() ) : the_post(); ?>
+                    <?php get_template_part( 'content-events', get_post_format() ); ?>
+                <?php endwhile; ?>
 
-                    <?php else : ?>
-                        <?php get_template_part( 'content-events', 'none' ); ?>
+                <?php else : ?>
+                    <?php get_template_part( 'content-events', 'none' ); ?>
 
-                <?php endif; // end have_posts() check ?>
-            </div>
+            <?php endif; // end have_posts() check ?>
         </div>
     </div>
     <div class="row">
@@ -47,19 +45,17 @@
     </div>
     <div class="row">
         <div class="small-12 medium-8 large-8 small-centered columns eventList removePadding">
-            <div class="panel">
-                <?php if ( have_posts() ) : ?>
+            <?php if ( have_posts() ) : ?>
 
-                    <?php /* Start the Loop */ ?>
-                    <?php while ( have_posts() ) : the_post(); ?>
-                        <?php get_template_part( 'content-pastevents', get_post_format() ); ?>
-                    <?php endwhile; ?>
+                <?php /* Start the Loop */ ?>
+                <?php while ( have_posts() ) : the_post(); ?>
+                    <?php get_template_part( 'content-pastevents', get_post_format() ); ?>
+                <?php endwhile; ?>
 
-                    <?php else : ?>
-                        <?php get_template_part( 'content-pastevents', 'none' ); ?>
+                <?php else : ?>
+                    <?php get_template_part( 'content-pastevents', 'none' ); ?>
 
-                <?php endif; // end have_posts() check ?>
-            </div>
+            <?php endif; // end have_posts() check ?>
         </div>
     </div>
 </div>
